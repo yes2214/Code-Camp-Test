0d27ac3afb11f416e8ca44d52d063354fa028c8d
yes2214 <ashtang216@gmail.com>
2025-07-03 14:20:57 -0700

    Update index.js

--- index.js
+++ index.js
@@ -9,9 +9,28 @@ app.listen(port, () =>){
 });
 console.log(`Example app listening at http://localhost:${port}`);
 });
-  <nav>
-    <a href="/"home.html"></a>
-    <a href="/"home2.html"></a>
-    <a href="/"home3.html"></a>
-  </nav>
+ const express = require('express');
+const app = express();
+const port  = 3000;
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/home.html');
+}
+app.listen(port, () =>){
+  res.sendFile(__dirname + '/home.html');
+});
+console.log(`Example app listening at http://localhost:${port}`);
+});
+const express = require('express');
+const app = express();
+const port  = 3000;
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/home.html');
+}
+app.listen(port, () =>){
+  res.sendFile(__dirname + '/home.html');
+});
+console.log(`Example app listening at http://localhost:${port}`);
+});
+ 
+
 
