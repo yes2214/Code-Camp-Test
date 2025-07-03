e17a4a3c8e82fb0e6385b229a5021557af4da424
yes2214 <ashtang216@gmail.com>
2025-07-03 14:26:33 -0700

    Update index.js

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ app.get('/', (req, res) => {
   res.sendFile(__dirname + '/index.html');
 }
 app.listen(port, () =>){
-  res.sendFile(__dirname + '/');
+  res.sendFile(__dirname + '/Strawberry Icecream');
 });
 console.log(`Example app listening at http://localhost:${port}`);
 });
@@ -27,7 +27,7 @@ app.get('/', (req, res) => {
   res.sendFile(__dirname + '/home3.html');
 }
 app.listen(port, () =>){
-  res.sendFile(__dirname + '/');
+  res.sendFile(__dirname + '/Some places to buy');
 });
 console.log(`Example app listening at http://localhost:${port}`);
 });
