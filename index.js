c77a8b97184359337e8ff7613a99a6b99ade1341
yes2214 <ashtang216@gmail.com>
2025-07-03 14:40:54 -0700

    Update index.js

--- index.js
+++ index.js
@@ -4,33 +4,17 @@ const port  = 3000;
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/index.html');
 }
-app.listen(port, () =>){
-  res.sendFile(__dirname + '/Strawberry Icecream');
-});
-console.log(`Example app listening at http://localhost:${port}`);
-});
- const express = require('express');
-const app = express();
-const port  = 3000;
+
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/home2.html');
 }
-app.listen(port, () =>){
-  res.sendFile(__dirname + '/Ingredients');
-});
-console.log(`Example app listening at http://localhost:${port}`);
-});
-const express = require('express');
-const app = express();
-const port  = 3000;
+
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/home3.html');
 }
 app.listen(port, () =>){
-  res.sendFile(__dirname + '/Some places to buy');
+  console.log(`Example app listening at http://localhost:${port}`);
 });
-console.log(`Example app listening at http://localhost:${port}`);
-});
- 
+
 
 
